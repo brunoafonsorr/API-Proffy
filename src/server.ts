b1f39c5e16b393cddd5cc8c926ad09d72a941ca6
brunoafonsorr <bruno.afonsorr.programador@gmail.com>
2024-07-03 12:28:34 -0300

    server-proffy fineshed

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
-app.use(express.json())
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 // GET: Buscar ou listar uma informação
 // POST: Criar alguma nova informação
@@ -11,13 +16,5 @@ app.use(express.json())
 // Corpo (Request Body): Dados para criação ou atualização de um registro
 // Route Params: Identificar qual recurso eu quero atualizar ou deletar
 // Querry Params: Paginação, filtros, organização
-
-app.get('/', (req, res) => {
-  return res.json({ mensage: 'Get Function' });
-})
-
-app.post('/', (req, res) => {
-  return res.json({ mensage: 'Post Function' });
-})
 
 app.listen(3333);
